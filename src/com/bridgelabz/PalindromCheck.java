3d44614afa873105aa69d4bd1c22feb6466738cd
Surabhi951 <surabhibhagat95@gmail.com>
2022-07-11 12:41:43 +0530

    add Palindrom check

---
+++ src/com/bridgelabz/PalindromCheck.java
@@ -0,0 +1,26 @@
+package com.bridgelabz;
+
+import java.util.Scanner;
+
+public class PalindromCheck {
+    public static void main(String[] args){
+        int n, c,r;u
+        int s=0;
+        System.out.print("Enter the Number");
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        c = n;
+
+        while (n > 0){
+            r = n%10;
+            s = (s*10)+r;
+            n = n/10;
+        }
+        if(c == s){
+            System.out.print("palindrom number");
+        }
+        else{
+            System.out.print("not a palindrom number");
+        }
+    }
+}
